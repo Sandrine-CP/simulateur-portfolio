35a066978acf5800db55156889bbc1478527a42e
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2024-11-06 00:44:01 +0100

    add : created filter form

--- src/components/PortfolioList.js
+++ src/components/PortfolioList.js
@@ -1,5 +1,18 @@
-function PortfolioList() {
-	return <div>PortfolioList component</div>;
+function PortfolioList({ portfolio, filter, setFilter }) {
+	return (
+		<div>
+			<label htmlFor="filter-select">Filtrer par :</label>
+			<select
+				id="filter-select"
+				value={filter}
+				onChange={(e) => setFilter(e.target.value)}
+			>
+				<option value="all">Toutes</option>
+				<option value="gains">Gains</option>
+				<option value="losses">Pertes</option>
+			</select>
+		</div>
+	);
 }
 
 export default PortfolioList;
