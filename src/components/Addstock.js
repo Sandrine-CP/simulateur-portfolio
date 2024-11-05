cee027438cb830775af1c0af5d9658dc9359eb33
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2024-11-05 23:53:00 +0100

    Addstock component finalized

--- src/components/Addstock.js
+++ src/components/Addstock.js
@@ -1,5 +1,32 @@
-function AddStock() {
-	return <div>AddStock component</div>;
+import { useState } from "react";
+
+function AddStock({ addStock }) {
+	// état du symbole de l'action
+	const [symbol, setSymbol] = useState("");
+
+	// fonction de submit du formulaire
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (symbol) {
+			addStock({ symbol, price: 0, gainloss: 0 });
+			setSymbol("");
+		}
+	};
+
+	return (
+		<>
+			<h2>Ajouter une nouvelle action</h2>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Ajouter un symbole d'action"
+					value={symbol}
+					onChange={(e) => setSymbol(e.target.value)}
+				/>
+				<button type="submit">Ajouter</button>
+			</form>
+		</>
+	);
 }
 
 export default AddStock;
